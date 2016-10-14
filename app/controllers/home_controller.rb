4adadda4c2f8582dde07efe4f416746bb5c82fba
Harry Nguyen <hamxvela@gmail.com>
2016-10-14 09:01:53 +0700

    Sort foods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,25 @@
 class HomeController < ApplicationController
   def index
   	section = params[:section]
+  	sort = params[:sort]
+
   	if section
   		@foods = Food.where(section: section)
   	else
   		@foods = Food.all
   	end
+
+  	if sort
+		case sort
+			when 'alphabetical'
+				@foods = @foods.order('name asc')
+			when 'low2high'
+				@foods = @foods.order('price asc')
+			when 'high2low'
+				@foods = @foods.order('price desc')
+		end
+  	end
+
   end
 
   def contact_us

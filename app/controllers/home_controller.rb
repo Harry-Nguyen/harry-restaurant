c646b576edcfe87ac6296f8b33147f02744942fd
Harry Nguyen <hamxvela@gmail.com>
2016-10-14 20:16:13 +0700

    Add search function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
   	section = params[:section]
   	sort = params[:sort]
+    search = params[:search]
 
   	if section
   		@foods = Food.where(section: section)
@@ -9,15 +10,24 @@ class HomeController < ApplicationController
   		@foods = Food.all
   	end
 
-  	if sort
-		case sort
-			when 'alphabetical'
-				@foods = @foods.order('name asc')
-			when 'low2high'
-				@foods = @foods.order('price asc')
-			when 'high2low'
-				@foods = @foods.order('price desc')
-		end
+  	if search
+      @foods.each do |food|
+        if food.name.include? search
+        else
+          @foods = @foods - [food]
+        end
+      end
+    end
+
+    if sort
+  		case sort
+  			when 'alphabetical'
+  				@foods = @foods.order('name asc')
+  			when 'low2high'
+  				@foods = @foods.order('price asc')
+  			when 'high2low'
+  				@foods = @foods.order('price desc')
+  		end
   	end
 
   end
